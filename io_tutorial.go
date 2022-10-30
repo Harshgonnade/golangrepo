ef45fae1a82155d56404ed2343bce876a6295d14
Harsh Gonnade <gonnadeharsh5@gmail.com>
2022-10-30 15:51:07 +0530

    added io_tutoria.go

---
+++ io_tutorial.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int
+	
+	// taking value from user
+	fmt.Scan(&x)
+	
+	// print variable value on console
+	fmt.Println(x)
+	
+	var y string
+	
+	fmt.Scan(&y)
+	
+	fmt.Println(y)
+	
+	
+}
