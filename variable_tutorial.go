483b0c8f67125477ebba3bc8c4a2e7e26c0f9fd4
Harsh Gonnade <gonnadeharsh5@gmail.com>
2022-10-30 15:27:03 +0530

    possible data type are added in comment in variable_tutorial.go

--- variable_tutorial.go
+++ variable_tutorial.go
@@ -7,6 +7,7 @@ func main() {
 	// Veriable decleration statement
 	// below mentioned how var declared in go lang
 	// syntax: var <name> <type>
+	// different types of data type are int,float32,float64,rune,string
 	var x int
 
 	// Variable assignment statement
