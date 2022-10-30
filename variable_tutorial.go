64aafcd4e1d8450477fda75fc9a0486e5cf1fb35
Harsh Gonnade <gonnadeharsh5@gmail.com>
2022-10-30 15:18:40 +0530

    added variable_tutorial.go file

---
+++ variable_tutorial.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Veriable decleration statement
+	// below mentioned how var declared in go lang
+	// syntax: var <name> <type>
+	var x int
+
+	// Variable assignment statement
+	x = 30
+
+	// printing variable on the console
+	fmt.Println(x)
+
+}
