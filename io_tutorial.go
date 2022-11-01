07c7c48de4614302b22a79c237d7693710450365
Harsh Gonnade <gonnadeharsh5@gmail.com>
2022-11-01 21:50:54 +0530

    i have added runu variable in this file

--- io_tutorial.go
+++ io_tutorial.go
@@ -7,16 +7,26 @@ func main() {
 	var x int
 	
 	// taking value from user
+	// it is use for enter input for user
 	fmt.Scan(&x)
 	
 	// print variable value on console
 	fmt.Println(x)
 	
+	// string take number and letters both
+	// string is a type of variable
 	var y string
 	
 	fmt.Scan(&y)
 	
 	fmt.Println(y)
+	
+	//
+	var z rune 
+	
+	fmt.Scan(&z)
+	
+	fmt.Println(z)
 	
 	
 }
