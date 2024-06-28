b3aefa195f33b7450b12d593554c07b32c537274
Harsh Gonnade <gonnadeharsh5@gmail.com>
2024-06-28 17:14:57 +0530

    my new file

--- looping_tutorial.go
+++ looping_tutorial.go
@@ -12,24 +12,27 @@ func main () {
 	// function calling statement
 	// input from user 
 	// syntax:- fmt.Scan<&variable name>
+	fmt.Print("Enter number:-	");
 	fmt.Scan(&a)
 	
 	// this loop is used for multiple operation
 	// syntax for loop is:- for <initialization>;<condition>;<post>{}
 	// like this 1) for i:=1 ; i<=20 ; i++{} 
 	for i := 1 ; i <= 10 ; i++ {
-		fmt.Println(a * i)
+		fmt.Println(a,"x",i,"=",a * i)
 	}
 	
 	//
+	fmt.Println("Integer Array");
 	numbers := []int{7, 9, 1, 2, 4, 5}
 
 	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
+		fmt.Println(numbers[i],i)
 
 	}
 	
 	//
+	fmt.Println("String Array");
 	arr := []string{"a", "b", "c", "d", "e", "f"}
 
 	for index, a := range arr {
